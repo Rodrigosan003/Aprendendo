d93be52b7fa978148314634458f91677a2360d49
Rodrigosan003 <r.santanaa003@gmail.com>
2025-05-23 17:43:14 -0300

    aulas de JS

---
+++ Curso de JavaScript/aula05/script.js
@@ -0,0 +1,13 @@
+let nome = "Rodrigo";
+let idade = 35;
+let estaAprovado = true;
+let sobrenome = undefined;
+let corSelecionado = null;
+
+let pessoa = {
+  nome: "Rodrigo",
+  idade: 35,
+  estaAprovado: true,
+  sobrenome: "Anastácio",
+};
+console.log(pessoa);
