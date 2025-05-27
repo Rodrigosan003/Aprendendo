3a401a6b03c6f0472600e6d9e4a10161a7c72085
Rodrigosan003 <r.santanaa003@gmail.com>
2025-05-27 20:39:31 -0300

    aulas de JS

--- Curso de JavaScript/README.md
+++
@@ -1,9 +0,0 @@
-# Curso Básico de JavaScript
-
-## Aulas:
-
-- Aula 01 - Console
-- Aula 02 - Variáveis
-- Aula 03 - Tipos Primitivos
-- Aula 04 - Tipagem Dinâmica
-- Aula 05 - Objetos
---
+++ Curso de JavaScript/aula07 - Functions/script.js
@@ -0,0 +1,16 @@
+let corSite = "Azul";
+function resetaCor() {
+  corSite = "";
+}
+
+console.log(corSite);
+resetaCor();
+console.log(corSite);
+
+let corSiten = "Vermelho";
+function resetaCorn(cor, tonalidade) {
+  corSiten = cor + tonalidade;
+}
+console.log(corSiten);
+resetaCorn("Verde", " claro");
+console.log(corSiten);
