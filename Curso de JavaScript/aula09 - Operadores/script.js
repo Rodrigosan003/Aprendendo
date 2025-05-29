3d87f1a3dd60eb9f9cf4f127f2081e70200d6755
Rodrigosan003 <r.santanaa003@gmail.com>
2025-05-29 16:58:05 -0300

    ajustes

---
+++ Curso de JavaScript/aula09 - Operadores/script.js
@@ -0,0 +1,15 @@
+// Operadores Aritméticos (Matemáticos).
+// +, -, *, /, **
+// ++, -- são operadores de incremento e decremento.
+
+let salario = 100;
+
+console.log(salario + salario);
+console.log(salario - salario);
+console.log(salario * salario);
+console.log(salario / salario);
+console.log(salario ** 2);
+// Operadores de Atribuição.
+// Operadores de Comparação.
+// Operadores Lógicos.
+// Operadores Bitwise.
