f69df1e87be95c02be0a07b940218114fb50e98a
Rodrigosan003 <r.santanaa003@gmail.com>
2025-06-04 16:19:16 -0300

    ajustes

--- Curso de JavaScript/aula09 - Operadores/script.js
+++ Curso de JavaScript/aula09 - Operadores/script.js
@@ -36,4 +36,19 @@
 // NOT = ! - Ele faz uma negação.
 // Comparações não boleanos
 // Falsy: undefined, null, 0, false, '', NaN
+// If... Else...
+// Se a hora estiver entre 06:00 até 12:00, bom dia!
+// Se estiver entre 12:00 até 18:00, boa tarde!
+// Caso contrário, boa noite.
+let hora = 18;
+
+if (hora >= 6 && hora < 12) {
+  console.log("Bom dia!");
+} else if (hora >= 12 && hora < 18) {
+  console.log("Boa tarde!");
+} else {
+  console.log("Boa noite!");
+}
+
+// Switch... case...
 // 6 - Operadores Bitwise.
