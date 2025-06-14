757fab7570a66c0c7baf602bc30142d116097a88
Rodrigosan003 <r.santanaa003@gmail.com>
2025-06-13 21:16:21 -0300

    ajustes

--- Curso de JavaScript/aula09 - Operadores/script.js
+++ Curso de JavaScript/aula09 - Operadores/script.js
@@ -98,20 +98,20 @@
 // } while (i < 10);
 
 // 4. For... in
-const pessoa = {
-  nome: "Rodrigo",
-  idade: 35,
-};
+// const pessoa = {
+//   nome: "Rodrigo",
+//   idade: 35,
+// };
 
-for (let chave in pessoa) {
-  console.log(chave, pessoa["nome"]);
-}
+// for (let chave in pessoa) {
+//   console.log(chave, pessoa["nome"]);
+// }
 
-const cores = ["vermelho", "azul", "verde"];
+// const cores = ["vermelho", "azul", "verde"];
 
-for (let indice in cores) {
-  console.log(indice, cores[indice]);
-}
+// for (let indice in cores) {
+//   console.log(indice, cores[indice]);
+// }
 
 // 5. For... of
 
