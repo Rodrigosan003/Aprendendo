26210bb50580be7502c1e0ca8256b5f361e2b9ca
Rodrigosan003 <r.santanaa003@gmail.com>
2025-06-16 20:08:20 -0300

    ajustes

---
+++ Curso de JavaScript/Mini-projetos/Máximo entre dois valores/script.js
@@ -0,0 +1,12 @@
+let valorMaior = max(1, 4);
+console.log(valorMaior);
+
+function max(num1, num2) {
+  //   if (num1 > num2) {
+  //     return num1;
+  //   } else {
+  //     return num2;
+  //   }
+
+  return num1 > num2 ? num1 : num2;
+}
