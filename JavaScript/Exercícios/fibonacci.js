0f25b791e90f222ea0602995487efae9ac0d759e
Rodrigosan003 <r.santanaa003@gmail.com>
2025-06-20 13:31:08 -0300

    exercício de fibonacci

---
+++ JavaScript/Exercícios/fibonacci.js
@@ -0,0 +1,10 @@
+function generateFibonaccis(n) {
+  let fib = [1, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+  return fib;
+}
+
+let fib = generateFibonaccis(20);
+console.log(fib.join(" "));
