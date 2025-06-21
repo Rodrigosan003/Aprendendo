c9ca3fbb358c8f0157838546723d5c4bcd5f1ef6
Rodrigosan003 <r.santanaa003@gmail.com>
2025-06-21 20:18:28 -0300

    exercício

---
+++ JavaScript/Exercícios/verificarIdade.js
@@ -0,0 +1,11 @@
+let idade = 112;
+
+if (idade < 18) {
+  console.log("Menor de idade!");
+} else if (idade >= 18) {
+  console.log("Maior de idade!");
+} else if (idade >= 60 && idade <= 100) {
+  console.log("Idoso!");
+} else {
+  console.log("Você está vivo ainda?");
+}
