7d3fc02b2a945bd07da154803d86087add0bb3c2
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-01 16:49:28 -0300

    prática

--- Cursos/TypeScript/Exercícios/01 - Básico/ex02.js
+++ Cursos/TypeScript/Exercícios/01 - Básico/ex02.js
@@ -1,8 +1,6 @@
 // Exercício 2: Função com tipagem explícita: Crie uma função soma(a: number, b: number): number que retorna a soma.
-
 // Sua solução aqui
-function soma(a: number, b: number) {
-  return a + b;
+function soma(a, b) {
+    return a + b;
 }
-
 console.log(soma(2, 5));
