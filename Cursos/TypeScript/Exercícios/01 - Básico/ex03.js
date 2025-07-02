0a6bcb387b10c0a1a75791d94dfe4d5e17a7a506
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-02 15:26:13 -0300

    pratica

---
+++ Cursos/TypeScript/Exercícios/01 - Básico/ex03.js
@@ -0,0 +1,7 @@
+// Exercício 3: Objeto com interface simples: Crie uma interface Pessoa e uma função que a utiliza.
+var usuario = {
+    nome: "Rodrigo",
+    idade: 35,
+    email: "email@contato.com",
+};
+console.log(usuario);
--- Cursos/TypeScript/Exercícios/01 - Básico/ex03.ts
+++ Cursos/TypeScript/Exercícios/01 - Básico/ex03.ts
@@ -1,3 +1,17 @@
 // Exercício 3: Objeto com interface simples: Crie uma interface Pessoa e uma função que a utiliza.
 
 // Sua solução aqui
+
+interface Pessoa {
+  nome: string;
+  idade: number;
+  email?: string; // ? Propriedade opcional.
+}
+
+const usuario: Pessoa = {
+  nome: "Rodrigo",
+  idade: 35,
+  email: "email@contato.com",
+};
+
+console.log(usuario);
