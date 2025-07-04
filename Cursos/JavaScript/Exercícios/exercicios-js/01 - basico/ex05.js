0d76ebba24c2dd99043cd2644bd8bf2208d5f975
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-04 16:36:34 -0300

    pratica

--- Cursos/JavaScript/Exercícios/exercicios-js/01 - basico/ex05.js
+++ Cursos/JavaScript/Exercícios/exercicios-js/01 - basico/ex05.js
@@ -1,1 +1,45 @@
 // 5 - Implemente uma função que calcule o fatorial de um número inteiro não negativo.
+// function fatorial(num) {
+//   let result = num;
+//   if (num === 0 || num === 1) {
+//     return 1;
+//   } else {
+//     while (num > 1) {
+//       num--;
+//       result *= num;
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(fatorial(0));
+
+// Função recursiva:
+function fatorialRecursivo(n) {
+  let resultado = 1;
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * fatorialRecursivo(n - 1);
+}
+
+// Função interativa:
+
+function fatorialInterativo(n) {
+  let resultado = 1;
+  for (let i = 2; i <= n; i++) {
+    resultado *= i;
+  }
+  return resultado;
+}
+
+const numero = 5;
+const fatorialRecursivoResultado = fatorialRecursivo(numero);
+const fatorialInterativoResultado = fatorialInterativo(numero);
+
+console.log(
+  `O resultado de ${numero} (recursivo) é: ${fatorialRecursivoResultado}`
+);
+console.log(
+  `O resultado de ${numero} (interativo) é: ${fatorialInterativoResultado}`
+);
--- Cursos/TypeScript/Exercícios/01 - Básico/ex05.js
+++ Cursos/TypeScript/Exercícios/01 - Básico/ex05.js
@@ -1,9 +1,5 @@
 // Exercício 5: Tupla simples: Crie uma tupla [string, number] com nome e idade.
-
 // Sua solução aqui
-
-let pessoa: [string, number];
-
+var pessoa;
 pessoa = ["Rodrigo", 35];
-
 console.log(pessoa);
