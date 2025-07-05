211a4b682926d7b8982acc319c6ffc1be508c708
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-05 09:09:27 -0300

    pratica

--- Cursos/JavaScript/Exercícios/exercicios-js/01 - basico/ex06.js
+++ Cursos/JavaScript/Exercícios/exercicios-js/01 - basico/ex06.js
@@ -1,1 +1,10 @@
 // 6 - Escreva uma função que converta uma temperatura de Celsius para Fahrenheit.
+function celciusParaFahrenheit(celcius) {
+  return (celcius * 9) / 5 + 32;
+}
+
+// Exemplo de uso:
+let temperaturaCelcius = 25;
+let temperaturaFahrenheit = celciusParaFahrenheit(temperaturaCelcius);
+
+console.log(`${temperaturaCelcius}Cº é igual a ${temperaturaFahrenheit}Fº.`);
