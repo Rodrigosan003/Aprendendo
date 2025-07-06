b9b45556035f6d62ae538381869859af1ef60141
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-06 08:47:02 -0300

    pratica

--- Cursos/TypeScript/Exercícios/01 - Básico/ex06.ts
+++ Cursos/TypeScript/Exercícios/01 - Básico/ex06.ts
@@ -1,3 +1,19 @@
 // Exercício 6: Enum simples: Enum com dias da semana e mensagens personalizadas.
 
 // Sua solução aqui
+
+enum DiaDaSemana {
+  Domingo,
+  Segunda,
+  Terca,
+  Quarta,
+  Quinta,
+  Sexta,
+  Sabado
+}
+
+let hoje: DiaDaSemana = DiaDaSemana.Terca;
+console.log(hoje); // Output: 2 (o valor padrão é numérico e começa em 0)
+
+let diaDaSemanaPorValor = DiaDaSemana[2];
+console.log(diaDaSemanaPorValor); // Output: Terca
