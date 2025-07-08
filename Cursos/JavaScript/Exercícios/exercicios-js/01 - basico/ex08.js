0f2e377495770bccfbcf5f807caad00c452d11b6
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-08 19:07:29 -0300

    pratica

--- Cursos/JavaScript/Exercícios/exercicios-js/01 - basico/ex08.js
+++ Cursos/JavaScript/Exercícios/exercicios-js/01 - basico/ex08.js
@@ -1,1 +1,12 @@
 // 8 - Verifique se uma string contém determinada palavra.
+
+function verificaJavaScript(str) {
+  return str.includes("javascript");
+}
+
+// Exemplo de uso:
+let texto1 = "Este é um exemplo de string com javascript.";
+let texto2 = "Esta string não contém a palavra.";
+
+console.log(verificaJavaScript(texto1)); // true
+console.log(verificaJavaScript(texto2)); // false
