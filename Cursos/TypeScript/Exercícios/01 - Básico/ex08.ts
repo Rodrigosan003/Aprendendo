e974ff530250ac06366c6e3d5c5a4488ace9510c
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-10 17:52:06 -0300

    pratica

--- Cursos/TypeScript/Exercícios/01 - Básico/ex08.ts
+++ Cursos/TypeScript/Exercícios/01 - Básico/ex08.ts
@@ -1,3 +1,19 @@
 // Exercício 8: Type Alias: Crie um type Status = 'ativo' | 'inativo' e use num objeto.
 
 // Sua solução aqui
+
+type Status = 'ativo' | 'inativo';
+
+interface ObjetoComStatus {
+  id: number;
+  nome: string;
+  status: Status;
+}
+
+const meuObjeto: ObjetoComStatus = {
+  id: 1,
+  nome: "Exemplo",
+  status: 'ativo'
+};
+
+console.log(meuObjeto);
