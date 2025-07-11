ec52d359048d2be5f596708a15848313b14075f9
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-11 18:46:24 -0300

    pratica

--- Cursos/TypeScript/Exercícios/01 - Básico/ex09.ts
+++ Cursos/TypeScript/Exercícios/01 - Básico/ex09.ts
@@ -1,3 +1,14 @@
 // Exercício 9: Type Assertion: Force um tipo em uma variável any para string.
 
 // Sua solução aqui
+
+let valor: any;
+
+valor = "Olá, mundo!";
+
+if (typeof valor === 'string') {
+    let str: string = valor as string;
+    console.log(str.toUpperCase());
+} else {
+    console.log("O valor não é uma string");
+}
