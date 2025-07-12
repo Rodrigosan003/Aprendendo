7808cbe3b79a87702a3dec4353b4fe24d41f5a9d
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-12 16:48:33 -0300

    pratica

--- Cursos/TypeScript/Exercícios/01 - Básico/ex10.ts
+++ Cursos/TypeScript/Exercícios/01 - Básico/ex10.ts
@@ -1,3 +1,14 @@
 // Exercício 10: Parâmetro opcional: Função que trata parâmetro opcional com mensagem diferente.
 
 // Sua solução aqui
+
+function saudar(nome?: string): string {
+  if (nome) {
+    return `Olá, ${nome}!`;
+  } else {
+    return "Olá, visitante!";
+  }
+}
+
+console.log(saudar("Maria")); // Saída: Olá, Maria!
+console.log(saudar()); // Saída: Olá, visitante!
