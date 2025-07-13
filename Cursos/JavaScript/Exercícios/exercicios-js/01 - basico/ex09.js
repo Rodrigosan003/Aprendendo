1c63dacc67b09dae144aa7c4b7ca652c8cf903d9
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-13 09:54:10 -0300

    pratica

--- Cursos/JavaScript/Exercícios/exercicios-js/01 - basico/ex09.js
+++ Cursos/JavaScript/Exercícios/exercicios-js/01 - basico/ex09.js
@@ -1,1 +1,16 @@
 // 9 - Crie uma função que receba um número e retorne se ele é múltiplo de 3 ou 5.
+
+const resultado = multiploNumero(2);
+console.log(resultado);
+
+function multiploNumero(entrada) {
+  if (entrada % 3 === 0 && entrada % 5 === 0) {
+    return `O número ${entrada} é múltipo de 3 e de 5.`;
+  } else if (entrada % 5 === 0) {
+    return `O número ${entrada} é múltipo de 5.`;
+  } else if (entrada % 3 === 0) {
+    return `O número ${entrada} é múltipo de 3.`;
+  } else {
+    return `O número ${entrada} não é múltiplo nem de 3 e nem de 5.`;
+  }
+}
