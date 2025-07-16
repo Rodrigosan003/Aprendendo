e8e69b148f716044e5ced2584acc2474dc7f49ee
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-16 17:39:23 -0300

    pratica

--- Cursos/JavaScript/Exercícios/exercicios-js/01 - basico/ex10.js
+++ Cursos/JavaScript/Exercícios/exercicios-js/01 - basico/ex10.js
@@ -1,1 +1,9 @@
 // 10 - Escreva um programa que imprima os números de 1 a 100, com "Fizz", "Buzz" e "FizzBuzz" (exercício clássico).
+
+for (let i = 1; i <= 100; i++) {
+  let output = "";
+  if (i % 2 === 0) output += "FizBuzz";
+  if (i % 3 === 0) output += "Fiz";
+  if (i % 5 === 0) output += "Buzz";
+  console.log(output || i);
+}
