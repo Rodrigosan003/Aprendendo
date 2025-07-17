4be2cdc9be020d58f1a2595d6818a5c9ecef51da
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-17 17:24:42 -0300

    pratica

--- Cursos/TypeScript/Exercícios/02 - Intermediario/ex01.ts
+++ Cursos/TypeScript/Exercícios/02 - Intermediario/ex01.ts
@@ -1,3 +1,19 @@
 // Exercício 1: Interface com propriedades opcionais e readonly.
 
 // Sua solução aqui
+
+interface User {
+  id: number;
+  name: string;
+  email?: string; // Propriedade opcional
+  readonly nickname: string; // Propriedade somente leitura
+}
+
+const user1: User = {
+  id: 1,
+  name: "Alice",
+  nickname: "ali",
+};
+
+user1.email = "alice@example.com"; // Ok
+// user1.nickname = "new_alias"; // Erro: Cannot assign to 'nickname' because it is a read-only property.
