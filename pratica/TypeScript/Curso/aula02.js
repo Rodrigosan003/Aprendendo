8d9d14d9059ac7d230b62d1d5c0fd9153c33a067
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-25 17:39:22 -0300

    pratica

---
+++ pratica/TypeScript/Curso/aula02.js
@@ -0,0 +1,3 @@
+var nome = "João";
+var idade = 30;
+console.log("Meu nome \u00E9 ".concat(nome, " e tenho ").concat(idade, " anos."));
---
+++ pratica/TypeScript/Curso/aula02.ts
@@ -0,0 +1,4 @@
+const nome = "João";
+const idade = 30;
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
