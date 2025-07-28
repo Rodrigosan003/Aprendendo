f2e7de566cedf79aa633bcfe130600ec1399f0c8
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-28 17:42:17 -0300

    aula4

---
+++ pratica/TypeScript/Curso/build/aula04.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome2 = "Rodrigo";
+let idade2 = 30;
+let ativo2 = true;
---
+++ pratica/TypeScript/Curso/src/aula04.ts
@@ -0,0 +1,5 @@
+let nome2: string = "Rodrigo";
+
+let idade2: number = 30;
+
+let ativo2: boolean = true;
