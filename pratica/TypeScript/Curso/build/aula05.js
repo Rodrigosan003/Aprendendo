5edd0a034c2b9ba3049a7c750bd1bb10a4e833cb
Rodrigosan003 <r.santanaa003@gmail.com>
2025-07-29 15:49:21 -0300

    aula5

---
+++ pratica/TypeScript/Curso/build/aula05.js
@@ -0,0 +1,9 @@
+"use strict";
+let cursos = ["JavaScript", "TypeScript", "C++"];
+let valores = [1, 2, 3, 4, 5];
+let vteste;
+vteste = "Teste";
+cursos.push("Python");
+valores.push(6);
+console.log(cursos);
+console.log(valores);
---
+++ pratica/TypeScript/Curso/src/aula05.ts
@@ -0,0 +1,11 @@
+let cursos = ["JavaScript", "TypeScript", "C++"];
+let valores = [1, 2, 3, 4, 5];
+let vteste: string | number;
+vteste = "Teste";
+// let cursos: string[] = [];
+
+cursos.push("Python");
+valores.push(6);
+
+console.log(cursos);
+console.log(valores);
