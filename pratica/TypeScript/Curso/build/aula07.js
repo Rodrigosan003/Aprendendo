b0f72fbd46a6263c078c64264c6181e5c8ac2f05
Rodrigosan003 <r.santanaa003@gmail.com>
2025-08-04 20:01:08 -0300

    aula07.ts

--- pratica/TypeScript/Curso/build/aula07.js
+++ pratica/TypeScript/Curso/build/aula07.js
@@ -1,7 +1,7 @@
-let coisas: [string, number, number, boolean] = ["teste", 10, 11, true];
+"use strict";
+let coisas = ["teste", 10, 11, true];
 console.log(coisas);
 console.log(coisas[0]);
 console.log(coisas[1]);
-
 coisas.push("Kit médico", 5, 6, false);
 console.log(coisas);
