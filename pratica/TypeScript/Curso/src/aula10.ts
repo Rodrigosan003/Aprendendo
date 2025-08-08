5a4d208c14eaced3ef6517b75b7a1f04020b374f
Rodrigosan003 <r.santanaa003@gmail.com>
2025-08-08 18:07:10 -0300

    aula10.ts

---
+++ pratica/TypeScript/Curso/src/aula10.ts
@@ -0,0 +1,14 @@
+// NULL - Nulo.
+// É um valor nulo.
+// let valorNulo: string;
+// console.log(valorNulo);
+
+// UNDEFINED - Indefinido.
+// É um valor indefinido.
+// let valorIndefinido: string | undefined;
+// console.log(valorIndefinido);
+
+// UNKNOWN - Desconhecido.
+// É um valor desconhecido.
+// let valorDesconhecido: unknown;
+// valorDesconhecido = 5;
