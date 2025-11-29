a9f323f88c732fae6d5e79dd36ded019b9f60b68
Rodrigosan003 <r.santanaa003@gmail.com>
2025-11-29 08:47:54 -0300

    pratica-JavaScript-praticasAleatorias

--- pratica/JavaScript/praticasAleatorias/somaTresNumeros.js
+++ pratica/JavaScript/praticasAleatorias/somaTresNumeros.js
@@ -1,5 +1,17 @@
+// const somaTresNumeros = (num1, num2, num3) => {
+//   return num1 + num2 + num3;
+// };
+
+// console.log(somaTresNumeros(3, 4, 2));
+
+const readline = require("readline-sync");
+
+let num1 = parseFloat(readline.question("Digite o primeiro número: "));
+let num2 = parseFloat(readline.question("Digite o segundo número: "));
+let num3 = parseFloat(readline.question("Digite o terceiro número: "));
+
 const somaTresNumeros = (num1, num2, num3) => {
   return num1 + num2 + num3;
-};
+}
 
-console.log(somaTresNumeros(3, 4, 2));
+console.log("A soma dos três números é: " + somaTresNumeros(num1, num2, num3));
