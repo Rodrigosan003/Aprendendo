e498fbdbe8e3da3b499d0c7b711aacd2f0dd9ea1
Rodrigosan003 <r.santanaa003@gmail.com>
2025-12-17 15:20:34 -0300

    pratica-Java-praticasAleatorias

---
+++ pratica/Java/Treinos/praticaAleatoria/Boolean.java
@@ -0,0 +1,10 @@
+public class Boolean {
+    public static void main(String[] args) {
+        boolean luz = false; // true ou false - valor booleano
+        if (luz == true){
+            System.out.println("Lampada acesa!");
+        } else {
+            System.out.println("Lampada apagada!");
+        }
+    }
+}
