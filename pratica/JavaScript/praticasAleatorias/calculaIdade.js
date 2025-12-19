2c8dc313d1dbe60f7035c1ab6db5827931e2303e
Rodrigosan003 <r.santanaa003@gmail.com>
2025-12-19 20:20:38 -0300

    pratica-Java-praticasAleatorias

---
+++ pratica/JavaScript/praticasAleatorias/calculaIdade.js
@@ -0,0 +1,12 @@
+const readline = require("readline-sync");
+
+let nome = readline.question("Digite seu nome: ");
+let anoDeNascimento = parseInt(readline.question("Digite o ano de nascimento: "));
+let anoAtual = parseInt(readline.question("Digite o ano atual: "));
+
+function calcularIdade(anoDeNascimento, anoAtual) {
+    return anoAtual - anoDeNascimento;
+}
+
+let idade = calcularIdade(anoDeNascimento, anoAtual);
+console.log(`Olá ${nome}. Sua idade é: ${idade} anos`);
