c58866e3a27afb2c977fefdfd58cb8b924f404d8
Rodrigosan003 <r.santanaa003@gmail.com>
2025-12-20 16:55:32 -0300

    pratica-JavaScript-praticasAleatorias

---
+++ pratica/JavaScript/praticasAleatorias/verificaIdade.js
@@ -0,0 +1,21 @@
+// Se for menor de 18 anos, exibir "Menor de idade"
+// Se for maior ou igual a 18 anos, exibir "Maior de idade"
+
+// Calcular idade.
+const readline = require("readline-sync");
+
+let nome = readline.question("Digite seu nome: ");
+let anoDeNascimento = parseInt(readline.question("Digite o ano de nascimento: "));
+let anoAtual = parseInt(readline.question("Digite o ano atual: "));
+
+function calcularIdade(anoDeNascimento, anoAtual) {
+    return anoAtual - anoDeNascimento;
+}
+
+let idade = calcularIdade(anoDeNascimento, anoAtual);
+
+if (idade < 18) {
+    console.log(`Olá ${nome}. Você é menor de idade.`);
+} else {
+    console.log(`Olá ${nome}. Você é maior de idade.`);
+}   
