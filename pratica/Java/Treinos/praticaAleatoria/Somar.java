6ae1123d748cfcacebd16684673912aa32a5ca81
Rodrigosan003 <r.santanaa003@gmail.com>
2025-12-22 18:03:25 -0300

    pratica-Java-praticasAleatorias

---
+++ pratica/Java/Treinos/praticaAleatoria/Somar.java
@@ -0,0 +1,8 @@
+public class Somar {
+    public static void main(String[] args) {
+        int n1 = 10; // valor inteiro
+        int n2 = 20; // valor inteiro
+        int soma = n1 + n2; // soma dos valores inteiros
+        System.out.println("A soma de " + n1 + " e " + n2 + " é: " + soma);
+    }
+}
