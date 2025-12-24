5e7b17462a4abac1203c2b34926cf701c4fe5c1b
Rodrigosan003 <r.santanaa003@gmail.com>
2025-12-24 11:45:42 -0300

    pratica-JavaScript-praticasAleatorias

---
+++ pratica/JavaScript/praticasAleatorias/contarPalavras.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Digite uma palavra: ', (palavra) => {
+    console.log(`A palavra "${palavra}" tem ${palavra.length} letras.`);
+    rl.close();
+});
+
