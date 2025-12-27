ae003774fd39ee2c61953ea1bae390eb8fa433a0
Rodrigosan003 <r.santanaa003@gmail.com>
2025-12-27 14:56:48 -0300

    pratica-TypeScript-praticasAleatorias

---
+++ pratica/TypeScript/praticaAleatoria/verificaIdade.js
@@ -0,0 +1,10 @@
+var idade = 12;
+if (idade >= 60) {
+    console.log("Você é idoso.");
+}
+else if (idade >= 18) {
+    console.log("Você é adulto.");
+}
+else {
+    console.log("Você é menor de idade.");
+}
---
+++ pratica/TypeScript/praticaAleatoria/verificaIdade.ts
@@ -0,0 +1,9 @@
+const idade: number = 12;
+
+if (idade >= 60) {
+    console.log("Você é idoso.");
+} else if (idade >= 18) {
+    console.log("Você é adulto.");
+} else {
+    console.log("Você é menor de idade.");
+}
