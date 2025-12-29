2e8021ed2a77559d96de8c98e60c378a7b82f868
Rodrigosan003 <r.santanaa003@gmail.com>
2025-12-29 18:18:51 -0300

    pratica-JavaScript-praticasAleatorias

---
+++ pratica/JavaScript/praticasAleatorias/arrayProdutos.js
@@ -0,0 +1,23 @@
+const brinquedos = {
+    nome: "Brinquedos",
+    produtos: [
+        {
+            nome: "Carrinho de Controle Remoto",
+            preco: 150.00,
+            descricao: "Carrinho de controle remoto com alta velocidade e design moderno.",
+            imagem: "caminho/para/imagem/carrinho.jpg"
+        },
+        {
+            nome: "Boneca Fashion",
+            preco: 80.00,
+            descricao: "Boneca com roupas fashion e acessórios variados.",
+            imagem: "caminho/para/imagem/boneca.jpg"
+        },
+        {
+            nome: "Quebra-Cabeça 3D",
+            preco: 60.00,
+            descricao: "Quebra-cabeça tridimensional para estimular a criatividade.",
+            imagem: "caminho/para/imagem/quebra-cabeca.jpg"
+        }
+    ] 
+}
