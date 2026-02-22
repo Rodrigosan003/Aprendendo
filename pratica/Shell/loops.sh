de28e9c37e776223f6a82b26b65ec91079f3a576
Rodrigosan003 <r.santanaa003@gmail.com>
2026-02-22 07:53:15 -0300

    práticando shell

---
+++ pratica/Shell/loops.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for i in 1 2 3 4 5; do
+    echo "Number: $i"
+done
+echo "-----------------"
+for file in *.txt; do
+    echo "Processing file: $file"
+done
+echo "-----------------"       
