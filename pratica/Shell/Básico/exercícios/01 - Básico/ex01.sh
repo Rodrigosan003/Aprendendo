71242783f9ab39915b712054be593671628558ae
Rodrigosan003 <r.santanaa003@gmail.com>
2026-02-26 17:41:29 -0300

    pratica-Shell

--- pratica/Shell/Básico/exercícios/01 - Básico/ex01.sh
+++ pratica/Shell/Básico/exercícios/01 - Básico/ex01.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 # EX01 - Crie um script que exiba a mensagem: "Olá, mundo!"
+echo "Olá, mundo!"
--- pratica/Shell/Básico/exercícios/01 - Básico/ex02.sh
+++ pratica/Shell/Básico/exercícios/01 - Básico/ex02.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 # EX02 - Crie um script que peça o nome do usuário e exiba: "Olá, <nome>!"
+echo "Digite seu nome:"
+read nome
+echo "Olá, $nome! Seja bem-vindo(a) ao mundo do Bash scripting!"
