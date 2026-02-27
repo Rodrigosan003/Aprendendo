4e8291706fd96ee4edd5ace65727752053f8cf90
Rodrigosan003 <r.santanaa003@gmail.com>
2026-02-27 17:33:19 -0300

    pratica-Shell

--- pratica/Shell/Básico/exercícios/01 - Básico/ex03.sh
+++ pratica/Shell/Básico/exercícios/01 - Básico/ex03.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 # EX03 - Crie um script que receba dois números e mostre a soma.
+# Solicita o primeiro número ao usuário
+echo "Digite o primeiro número:"
+read num1
+# Solicita o segundo número ao usuário
+echo "Digite o segundo número:"
+read num2
+# Calcula a soma dos dois números
+soma=$((num1 + num2))
+# Exibe o resultado da soma
+echo "A soma de $num1 e $num2 é: $soma"
