680c0f5173a64d50f9c032e59fcdd9be394ad7e9
Rodrigosan003 <r.santanaa003@gmail.com>
2026-02-28 16:44:16 -0300

    pratica-Shell

--- pratica/Shell/Básico/exercícios/01 - Básico/ex04.sh
+++ pratica/Shell/Básico/exercícios/01 - Básico/ex04.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 # EX04 - Crie um script que informe se um número é par ou ímpar.
+echo "Digite um número:"
+read numero
+# Verifica se o número é par ou ímpar usando o operador de módulo
+if (( numero % 2 == 0 )); then
+    echo "O número $numero é par."
+else
+    echo "O número $numero é ímpar."
+fi
