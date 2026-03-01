0160dea04b4e113e189da0ee52432793dce5393d
Rodrigosan003 <r.santanaa003@gmail.com>
2026-03-01 06:10:52 -0300

    pratica-Shell

--- pratica/Shell/Básico/exercícios/01 - Básico/ex05.sh
+++ pratica/Shell/Básico/exercícios/01 - Básico/ex05.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 # EX05 - Crie um script que mostre todos os arquivos do diretório atual.
+echo "Arquivos no diretório atual:"
+# Lista todos os arquivos no diretório atual usando o comando ls
+ls -a
