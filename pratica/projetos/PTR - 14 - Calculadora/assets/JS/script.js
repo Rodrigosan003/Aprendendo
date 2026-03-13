323e21c37d3d52a9122a3dc0b76d1bfc552e4276
Rodrigosan003 <r.santanaa003@gmail.com>
2026-03-13 16:06:24 -0300

    pratica-projetos-PTR 14

--- pratica/projetos/PTR - 14 - Calculadora/assets/JS/script.js
+++ pratica/projetos/PTR - 14 - Calculadora/assets/JS/script.js
@@ -1,3 +1,8 @@
 let first = document.getElementById("first");
 let second = document.getElementById("second");
-
+let adicao = document.getElementById("adicao");
+let subtracao = document.getElementById("subtracao");
+let multiplicacao = document.getElementById("multiplicacao");
+let divisao = document.getElementById("divisao");
+let limpar = document.getElementById("limpar");
+let igual = document.getElementById("igual");
