3b6daa6bc022dfc6fe9df448d1e39287ed2777e2
Rodrigosan003 <r.santanaa003@gmail.com>
2026-03-17 09:28:24 -0300

    pratica-projetos-PTR 14

---
+++ pratica/projetos/PTR - 14 - Calculadora-bkp/assets/JS/script.js
@@ -0,0 +1,29 @@
+// Inputs
+let first = document.getElementById("first");
+let second = document.getElementById("second");
+
+// Operação
+let adicao = document.getElementById("adicao");
+let subtracao = document.getElementById("subtracao");
+let multiplicacao = document.getElementById("multiplicacao");
+let divisao = document.getElementById("divisao");
+
+// Ação
+let limpar = document.getElementById("limpar");
+let igual = document.getElementById("igual");
+
+let res = document.getElementById("res");
+
+function clicou() {
+  return res.textContent = "Clicou!!!";
+};
+
+
+
+
+
+
+
+
+
+
