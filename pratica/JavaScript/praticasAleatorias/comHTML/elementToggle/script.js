0e9c43a5333c8c6a93c47a80c0ea611a47a862f3
Rodrigosan003 <r.santanaa003@gmail.com>
2026-03-20 20:11:47 -0300

    eventToggle

--- pratica/JavaScript/praticasAleatorias/comHTML/elementToggle/script.js
+++ pratica/JavaScript/praticasAleatorias/comHTML/elementToggle/script.js
@@ -1,6 +1,11 @@
-let botao = document.querySelector("#botao");
+let btnRed = document.querySelector("#btn-red");
+let btnBlue = document.querySelector("#btn-blue");
 let body = document.querySelector("body");
 
-botao.addEventListener("click", () => {
+btnRed.addEventListener("click", () => {
     body.classList.toggle("red");
+})
+
+btnBlue.addEventListener("click", () => {
+    body.classList.toggle("blue")
 })
