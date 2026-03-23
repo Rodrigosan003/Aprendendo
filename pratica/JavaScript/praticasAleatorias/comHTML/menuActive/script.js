99657d9c73a05133fec9f98e671721ddfe92444b
Rodrigosan003 <r.santanaa003@gmail.com>
2026-03-23 11:44:31 -0300

    pratica-JavaScript-praticaAleatorias-comHTML

--- pratica/JavaScript/praticasAleatorias/comHTML/menuActive/script.js
+++ pratica/JavaScript/praticasAleatorias/comHTML/menuActive/script.js
@@ -1,3 +1,13 @@
 let links = document.querySelectorAll("nav a");
 
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        
+        // remove de todos
+        links.forEach((l) => l.classList.remove("active"));
+
+        // adiciona no clicado
+        link.classList.add("active");
+    });
+});
 
