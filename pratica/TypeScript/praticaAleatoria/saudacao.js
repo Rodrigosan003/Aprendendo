f70792c6f4e4adcea2a50b2b0e72281e45057f46
Rodrigosan003 <r.santanaa003@gmail.com>
2026-03-25 20:16:06 -0300

    pratica-TypeScript-praticaAleatória

---
+++ pratica/TypeScript/praticaAleatoria/saudacao.js
@@ -0,0 +1,3 @@
+var nome = "Rodrigo";
+var idade = 35;
+console.log("Ol\u00E1! Eu sou ".concat(nome, " e tenho ").concat(idade, " anos."));
---
+++ pratica/TypeScript/praticaAleatoria/saudacao.ts
@@ -0,0 +1,4 @@
+const nome:string = "Rodrigo";
+const idade:number = 35;
+
+console.log(`Olá! Eu sou ${nome} e tenho ${idade} anos.`);
