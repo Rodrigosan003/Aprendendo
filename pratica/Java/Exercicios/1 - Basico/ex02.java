d63bb7fe63d2afcf7f84dd51b0f77e1174939827
Rodrigosan003 <r.santanaa003@gmail.com>
2026-03-31 18:06:50 -0300

    pratica-Java-Aulas-primeiro-projeto

--- pratica/Java/Exercicios/1 - Basico/ex02.java
+++ pratica/Java/Exercicios/1 - Basico/ex02.java
@@ -4,5 +4,10 @@
 public class Ex02 {
     public static void main(String[] args) {
         // implemente aqui
+        int numero1 = 12;
+        int numero2 = 12;
+        int soma = numero1 + numero2;
+
+        System.out.println(soma);
     }
 }
