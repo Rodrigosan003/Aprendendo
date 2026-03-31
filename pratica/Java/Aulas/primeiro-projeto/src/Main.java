64f0157518c54915e0778576b3e7835f9adb226d
Rodrigosan003 <r.santanaa003@gmail.com>
2026-03-31 17:42:08 -0300

    pratica-Java-Aulas-primeiro-projeto

--- pratica/Java/Aulas/primeiro-projeto/src/Main.java
+++ pratica/Java/Aulas/primeiro-projeto/src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Instrução número 1");
-        System.out.println("Instrução número 2");
-        System.out.println("Instrução número 3");
-        System.out.println("Instrução número 4");
+        // System.out.println("Instrução número 1");
+        // System.out.println("Instrução número 2");
+        // System.out.println("Instrução número 3");
+        // System.out.println("Instrução número 4");
+
+        // Tipos primitivos.
+
+        int idade = 25;
+        double salarioMensal = 4500.00;
     }
 }
