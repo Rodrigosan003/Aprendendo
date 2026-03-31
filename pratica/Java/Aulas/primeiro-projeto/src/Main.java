178e2bae4261dfb39f5112518a1bfa9be595d90b
Rodrigosan003 <r.santanaa003@gmail.com>
2026-03-31 15:51:34 -0300

    pratica-Java-Aulas-primeiro-projeto

---
+++ pratica/Java/Aulas/primeiro-projeto/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Instrução número 1");
+        System.out.println("Instrução número 2");
+        System.out.println("Instrução número 3");
+        System.out.println("Instrução número 4");
+    }
+}
