720c9fbfa8959bedb35589925a797241d65c4fd2
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-02 18:36:02 -0300

    pratica-Java-Treinos e Exercícios

---
+++ pratica/Java/Treinos/VerificaPar-Impar/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero = 10;
+
+        if (numero % 2 == 0) {
+            System.out.println("O número " + numero + " é Par.");
+        }
+        else {
+            System.out.println("O número " + numero + "é Ímpar.");
+        }
+    }
+}
