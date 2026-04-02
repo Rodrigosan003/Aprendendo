55811fca194f688201983b1b9d575f9fd6eb56f3
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-02 11:15:45 -0300

    pratica-Java-Aulas]

---
+++ pratica/Java/Aulas/primeiro-projeto/src/EntradaSaidaDeDados.java
@@ -0,0 +1,7 @@
+public class EntradaSaidaDeDados {
+    public static void main(String[] args) {
+        System.out.println("Olá, eu faço impressão e pulo a linha.");
+        System.out.print("Eu faço a impressão na mesma linha.");
+        System.out.printf("Olá, eu faço a impressão formatada.");
+    }
+}
