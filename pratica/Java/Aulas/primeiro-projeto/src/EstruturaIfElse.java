5f913d5c2b02b0f35d2a1a8245de3fa47c073559
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-02 18:01:53 -0300

    pratica-Java-Aulas

--- pratica/Java/Aulas/primeiro-projeto/src/EstruturaIfElse.java
+++ pratica/Java/Aulas/primeiro-projeto/src/EstruturaIfElse.java
@@ -1,12 +1,21 @@
 public class EstruturaIfElse {
     public static void main(String[] args) {
-        int idade = 10;
+//        int idade = 10;
+//
+//        if (idade >= 18) {
+//            System.out.println("Você é maior de idade.");
+//        }
+//        else {
+//            System.out.println("Você é menor de idade.");
+//        }
 
-        if (idade >= 18) {
-            System.out.println("Você é maior de idade.");
+        boolean estaChovendo = true;
+
+        if (!estaChovendo) {
+            System.out.println("Vamos sair para passear.");
         }
         else {
-            System.out.println("Você é menor de idade.");
+            System.out.println("Vamos ficar em casa.");
         }
     }
 }
