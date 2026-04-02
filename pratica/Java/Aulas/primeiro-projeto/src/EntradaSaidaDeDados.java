36a80b14a58dfb6d25a8f00be1a9aaa07acb00a7
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-02 11:27:07 -0300

    pratica-Java-Aulas

--- pratica/Java/Aulas/primeiro-projeto/src/EntradaSaidaDeDados.java
+++ pratica/Java/Aulas/primeiro-projeto/src/EntradaSaidaDeDados.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class EntradaSaidaDeDados {
     public static void main(String[] args) {
-        System.out.println("Olá, eu faço impressão e pulo a linha.");
-        System.out.print("Eu faço a impressão na mesma linha.");
-        System.out.printf("Olá, eu faço a impressão formatada.");
+        //System.out.println("Olá, eu faço impressão e pulo a linha.");
+        //System.out.print("Eu faço a impressão na mesma linha.");
+        //System.out.printf("Olá, eu faço a impressão formatada.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá, digite o seu nome: ");
+        String nome = scanner.nextLine();
     }
 }
