4fdd7393da65500e020757cd3a686cb481de9ba7
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-03 11:11:54 -0300

    pratica-Java-Aula

--- pratica/Java/Aulas/primeiro-projeto/src/While.java
+++ pratica/Java/Aulas/primeiro-projeto/src/While.java
@@ -2,9 +2,14 @@ public class While {
     public static void main(String[] args) {
         int contador = 0;
 
-        while (contador <5) {
+//        while (contador <5) {
+//            System.out.println("Contador: " + contador);
+//            contador++;
+//        }
+
+        do {
             System.out.println("Contador: " + contador);
             contador++;
-        }
+        }while (contador < 5);
     }
 }
