a660d3fef6d19cdf6cc2dac1fe62df32f12ac7e7
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-03 10:39:10 -0300

    pratica-Java-Aula

--- pratica/Java/Aulas/primeiro-projeto/src/SwitchCase.java
+++ pratica/Java/Aulas/primeiro-projeto/src/SwitchCase.java
@@ -27,5 +27,21 @@ public class SwitchCase {
             default:
                 System.out.println("Dia inválido!!!");
         };
+
+        // Outra forma Melhorada:
+
+        if (diaDaSemana < 1 || diaDaSemana > 7) {
+            System.out.println("Dia inválido!!!");
+        } else {
+            switch (diaDaSemana) {
+                case 1: System.out.println("Domingo!"); break;
+                case 2: System.out.println("Segunda!"); break;
+                case 3: System.out.println("Terça!"); break;
+                case 4: System.out.println("Quarta!"); break;
+                case 5: System.out.println("Quinta!"); break;
+                case 6: System.out.println("Sexta!"); break;
+                case 7: System.out.println("Sábado!"); break;
+            }
+        }
     }
 }
