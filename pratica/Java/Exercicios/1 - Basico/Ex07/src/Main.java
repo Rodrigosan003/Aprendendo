bfb8f1c4c37151c1d126ba1a2adaa31163adfc37
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-06 13:51:53 -0300

    pratica-Java-Exercícios

---
+++ pratica/Java/Exercicios/1 - Basico/Ex07/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        //O dobro do número
+        int dobroNumero = numero * 2;
+        System.out.println("O dobro do número " + numero + " é: " + dobroNumero);
+    }
+}
