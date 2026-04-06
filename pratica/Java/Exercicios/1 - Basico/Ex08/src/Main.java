d598fa7449d045d89064b26232d99ed7bcab2937
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-06 14:10:54 -0300

    pratica-Java-Exercícios

---
+++ pratica/Java/Exercicios/1 - Basico/Ex08/src/Main.java
@@ -0,0 +1,9 @@
+// 8. Crie um contador de 1 a 10 usando for.
+
+public class Main {
+    public static void main(String[] args) {
+        for (int cont = 0; cont <= 10; cont++) {
+            System.out.println("Número: " + cont);
+        }
+    }
+}
