ce54c014ee97bd384b00e7d2b8d065368b773e61
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-07 16:46:17 -0300

    pratica-Java-Treino

--- pratica/Java/Treinos/Idade/src/Main.java
+++ pratica/Java/Treinos/Idade/src/Main.java
@@ -12,8 +12,18 @@ public class Main {
             System.out.println("Surpreendente!!! Você ainda existe?");
         } else if (idade <= 129 && idade >= 100) {
             System.out.println("Parabéns por chegar até aqui!!! Você tem " + "anos. Você é uma raridade.");
-        } else if (idade > 60) {
-            System.out.println();
+        } else if (idade >= 60) {
+            System.out.println("Você é um idoso. Se cuide.");
+        } else if (idade < 60 && idade >= 30) {
+            System.out.println("Você está no Alge. Aproveite para se cuidar bastante.");
+        } else if (idade < 30 && idade >= 18) {
+            System.out.println("Você está na juventude. Cuide da sua capacitação pessoa.");
+        } else if (idade < 18) {
+            System.out.println("Adolecente!!! Só estude e se qualifique.");
+        } else if (idade < 10) {
+            System.out.println("Criança!!! Vá brincar com coisas saldáveis.");
+        } else {
+            System.out.println("Por favor... Digite uma idade válida!!!");
         }
     }
 }
