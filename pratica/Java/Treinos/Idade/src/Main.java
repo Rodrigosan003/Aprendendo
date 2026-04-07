7250044197c2c169ea34508ca68f3c2e7a14c1bd
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-07 16:38:36 -0300

    pratica-Java-Treino

--- pratica/Java/Treinos/Idade/src/Main.java
+++ pratica/Java/Treinos/Idade/src/Main.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
-        Scanner scanner = new Sca
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        // Verificação
+        if (idade > 130) {
+            System.out.println("Surpreendente!!! Você ainda existe?");
+        } else if (idade <= 129 && idade >= 100) {
+            System.out.println("Parabéns por chegar até aqui!!! Você tem " + "anos. Você é uma raridade.");
+        } else if (idade > 60) {
+            System.out.println();
+        }
     }
 }
