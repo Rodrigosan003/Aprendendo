3c3c1deb8ca005c949080907e088d8f8b8bac447
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-10 16:41:12 -0300

    pratica-Java-Treinos-praticaAleatoria

--- pratica/Java/Treinos/praticaAleatoria/calculaIdade/src/Main.java
+++ pratica/Java/Treinos/praticaAleatoria/calculaIdade/src/Main.java
@@ -1,7 +1,20 @@
-import java.utils.Scanner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite seu ano de nascimento: ");
+        int nascimento = scanner.nextInt();
+        System.out.println("Digite o ano atual: ");
+        int anoAtual = scanner.nextInt();
+
+        // Calcular idade.
+        int idade = anoAtual - nascimento;
+        String mensagem = "Olá " + nome + " você nasceu no ano de " + nascimento + " e no ano de " + anoAtual + " você tem " + idade + " anos.";
+
+        System.out.println(mensagem);
     }
 }
