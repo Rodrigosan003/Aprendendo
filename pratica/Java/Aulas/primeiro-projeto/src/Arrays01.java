d54dfc1a51d5e75c3fabf38090b4599b94a39bb5
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-14 15:33:05 -0300

    pratica-Java-Aulas

--- pratica/Java/Aulas/primeiro-projeto/src/Arrays01.java
+++ pratica/Java/Aulas/primeiro-projeto/src/Arrays01.java
@@ -1,6 +1,9 @@
 public class Arrays01 {
     public static void main(String[] args) {
         int [] idades = new int[3]; // Arrays é uma variável do tipo reference.
-        System.out.println(idades);
+        idades[0] = 21;
+        idades[1] = 15;
+        idades[2] = 11;
+        System.out.println(idades[0]);
     }
 }
