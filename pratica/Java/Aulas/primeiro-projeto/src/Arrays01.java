d12789de9c9f2534df581232112c683d929d347b
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-14 15:34:10 -0300

    pratica-Java-Aulas

--- pratica/Java/Aulas/primeiro-projeto/src/Arrays01.java
+++ pratica/Java/Aulas/primeiro-projeto/src/Arrays01.java
@@ -5,5 +5,7 @@ public class Arrays01 {
         idades[1] = 15;
         idades[2] = 11;
         System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
     }
 }
