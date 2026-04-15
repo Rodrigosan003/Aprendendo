d0e687b7c55c3201f326ca4446e25a3a6141c739
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-15 16:27:05 -0300

    pratica-Java-Aulas

--- pratica/Java/Aulas/primeiro-projeto/src/Arrays03.java
+++ pratica/Java/Aulas/primeiro-projeto/src/Arrays03.java
@@ -2,5 +2,9 @@ public class Arrays03 {
     public static void main(String[] args) {
 
         String[] nomes = new String[3];
+
+        nomes[0] = "Goku";
+        nomes[1] = "Satã";
+        nomes[2] = "Videl";
     }
 }
