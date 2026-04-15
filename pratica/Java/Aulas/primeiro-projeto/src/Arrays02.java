170ff21c08359b99f97555f3866b28cd48b0c0f0
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-15 16:30:06 -0300

    pratica-Java-Aulas

--- pratica/Java/Aulas/primeiro-projeto/src/Arrays02.java
+++ pratica/Java/Aulas/primeiro-projeto/src/Arrays02.java
@@ -1,9 +1,6 @@
 public class Arrays02 {
     public static void main(String[] args) {
-        // byte, short int, long, float e double 0
-        // char '\u000' = ''
-        // boolean false
-        // String null
+     
 
         int [] idades = new int[3];
 
