e8e1eb53142fa3b53ae17c021902567b9c4e1da1
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-16 17:17:34 -0300

    pratica-Java-Aulas

--- pratica/Java/Aulas/primeiro-projeto/src/Arrays04.java
+++ pratica/Java/Aulas/primeiro-projeto/src/Arrays04.java
@@ -2,6 +2,7 @@ public class Arrays04 {
     public static void main(String[] args) {
         int[] numeros = new int[3];
         int[] numeros2 = {1, 2, 3, 4, 5};
+        int[] numeros3 = new int[] {1, 2, 3, 4, 5, 6};
 
         for (int i = 0; i < numeros2.length; i++) {
             System.out.println(numeros2[i]);
