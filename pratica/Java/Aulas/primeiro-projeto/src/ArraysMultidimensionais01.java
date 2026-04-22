13f56e57f35a0f094c1d778105820dce94e0419b
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-22 14:08:38 -0300

    pratica-Java-Aulas

--- pratica/Java/Aulas/primeiro-projeto/src/ArraysMultidimensionais01.java
+++ pratica/Java/Aulas/primeiro-projeto/src/ArraysMultidimensionais01.java
@@ -1,5 +1,15 @@
 public class ArraysMultidimensionais01 {
     public static void main(String[] args) {
         int[][] dias = new int[3][3];
+
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 31;
+        dias[1][1] = 28;
+        dias[1][2] = 31;
+
+        
     }
 }
