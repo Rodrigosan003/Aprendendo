fefce6fd81dc0794f7c662a593c67c025fe6cc9b
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-25 17:38:24 -0300

    pratica-JavaScript-praticasAleatorias-comHTML

--- pratica/JavaScript/praticasAleatorias/comHTML/texto/script.js
+++ pratica/JavaScript/praticasAleatorias/comHTML/texto/script.js
@@ -1,8 +1,14 @@
 let res = document.querySelector("#res");
 let entrada = document.querySelector("#entrada");
+let btn = document.querySelector("#btn");
 // function botao() {
 //     // alert("clicou");
 //     res.textContent = entrada.value;
 //     entrada.value = "";
 // }
+
+btn.addEventListener('click', () => {
+    res.textContent = entrada.value;
+    entrada.value = "";
+})
 
