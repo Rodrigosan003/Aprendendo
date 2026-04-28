27088c0ca135edb2ef5481717378559c26891abc
Rodrigosan003 <r.santanaa003@gmail.com>
2026-04-28 09:08:32 -0300

    pratica-Java-Aulas

--- pratica/Java/Aulas/primeiro-projeto/src/ArraysMultidimensionais2.java
+++ pratica/Java/Aulas/primeiro-projeto/src/ArraysMultidimensionais2.java
@@ -2,7 +2,9 @@ public class ArraysMultidimensionais2 {
     public static void main(String[] args) {
         int[][] arrayInt = new int[3][];
         for (int[] arrayBase: arrayInt){
-            
+            for (int num: arrayBase) {
+                
+            }
         }
     }
 }
