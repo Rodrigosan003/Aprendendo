25f58df5c437c28c5b5a8d7e2e44f094dec5f477
Rodrigosan003 <r.santanaa003@gmail.com>
2026-05-20 16:56:10 -0300

    pratica-JavaScript-praticasAleatorias-botaoInterativo2

---
+++ pratica/JavaScript/praticasAleatorias/comHTML/botaoInterativo2/script.js
@@ -0,0 +1,7 @@
+let container = document.querySelector(".container");
+
+let blue = document.querySelector("#blue");
+let red = document.querySelector("#red");
+let black = document.querySelector("#black");
+let green = document.querySelector("#green");
+
