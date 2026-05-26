78d93ffb14056f50176eea45504583dff494da26
Rodrigosan003 <r.santanaa003@gmail.com>
2026-05-26 18:41:28 -0300

    pratica-projetos-PTR 04

--- pratica/projetos/PTR - 04 - Desafios da RocketSeat./7 - Desafio - Theme Switcher/assets/js/script.js
+++ pratica/projetos/PTR - 04 - Desafios da RocketSeat./7 - Desafio - Theme Switcher/assets/js/script.js
@@ -1,10 +1,6 @@
 let container = document.querySelector(".container");
 let ckb = document.querySelector("#ckb");
 
-// Função teste só para ver o funcionamento.
-// function teste() {
-//     container.classList.toggle("dark-mode");
-// }
 
 ckb.addEventListener("click", () => {
     container.classList.toggle("dark-mode");
