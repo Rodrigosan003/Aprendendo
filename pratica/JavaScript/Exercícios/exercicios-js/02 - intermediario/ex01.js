98e7600fcf7edea7319a50eaa00fe19246190b0e
Rodrigosan003 <r.santanaa003@gmail.com>
2026-06-17 14:29:09 -0300

    pratica-JavaScript-Exercícios

--- pratica/JavaScript/Exercícios/exercicios-js/02 - intermediario/ex01.js
+++ pratica/JavaScript/Exercícios/exercicios-js/02 - intermediario/ex01.js
@@ -1,1 +1,8 @@
 // 01 - Crie uma função que receba uma string e retorne a mesma string invertida.
+
+function inverteString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Exemplo de uso:
+// console.log(inverteString('Olá mundo'));
