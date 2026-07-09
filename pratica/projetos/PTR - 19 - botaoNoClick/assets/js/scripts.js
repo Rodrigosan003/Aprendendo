022da6f5fbc2b177567459109708ca3039a54ead
Rodrigosan003 <r.santanaa003@gmail.com>
2026-07-09 18:03:15 -0300

    pratica-projetos-PTR 19

--- pratica/projetos/PTR - 19 - botaoNoClick/assets/js/scripts.js
+++ pratica/projetos/PTR - 19 - botaoNoClick/assets/js/scripts.js
@@ -1,5 +1,7 @@
 let title = document.querySelector("h1");
+let body = document.querySelector("body");
 
 function clicou(){
     title.textContent = "Eu falei para não clicar!"
+    body.classList.toggle("red")
 }
