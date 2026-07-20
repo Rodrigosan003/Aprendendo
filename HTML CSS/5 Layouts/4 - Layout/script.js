76ea3425dbd86897f10393689078803204abb1a1
Rodrigosan003 <r.santanaa003@gmail.com>
2026-07-20 14:04:38 -0300

    HTML CSS-5 Layouts

--- HTML CSS/5 Layouts/4 - Layout/script.js
+++ HTML CSS/5 Layouts/4 - Layout/script.js
@@ -1,9 +1,11 @@
-openMenu.addEventListener('click', () => {
-    menu.style.display = 'flex';
-    menu.style.opacity = '1';
-})
+openMenu.addEventListener("click", () => {
+  menu.style.display = "flex";
+  setTimeout(() => {
+    menu.style.opacity = "1";
+  }, 10);
+});
 
-closeMenu.addEventListener('click', () => {
-    menu.style.opacity = '0';
-    menu.removeAttribute('style');
-})
+closeMenu.addEventListener("click", () => {
+  menu.style.opacity = "0";
+  menu.removeAttribute("style");
+});
