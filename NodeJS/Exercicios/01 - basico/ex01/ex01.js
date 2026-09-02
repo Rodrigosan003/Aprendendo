fd57957fdb450379c9652ce85be6d7085cc6399f
Rodrigosan003 <r.santanaa003@gmail.com>
2026-09-02 15:39:21 -0300

    Node-Exercicios-ex01

--- NodeJS/Exercicios/01 - basico/ex01/ex01.js
+++ NodeJS/Exercicios/01 - basico/ex01/ex01.js
@@ -1,3 +1,9 @@
 // Exercício 1: Criar um servidor HTTP simples que responda 'Olá, mundo!'.
 
 const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/plain; charset=utf-8"
+    });
+})
