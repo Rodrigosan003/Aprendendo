5dc31116f22c793547a586787fed44e2594f2b2d
Rodrigosan003 <r.santanaa003@gmail.com>
2026-09-02 15:42:17 -0300

    Node-Exercicios-ex01

--- NodeJS/Exercicios/01 - basico/ex01/ex01.js
+++ NodeJS/Exercicios/01 - basico/ex01/ex01.js
@@ -8,4 +8,8 @@ const server = http.createServer((req, res) => {
     });
 
     res.end("Olá Mundo!");
-})
+});
+
+server.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+});
