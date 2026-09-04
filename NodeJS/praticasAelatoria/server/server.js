007aee86f57fcb7b19bd75bba4749c544898abc4
Rodrigosan003 <r.santanaa003@gmail.com>
2026-09-04 17:44:54 -0300

    NodeJs-praticasAleatorias-server

--- NodeJS/praticasAelatoria/server/server.js
+++ NodeJS/praticasAelatoria/server/server.js
@@ -3,5 +3,6 @@ const https = require("http");
 const server = https.createServer((req, res) => {
     res.writeHead(200, {
         "Content-Type": "text/plain; charset=utf-8"
-    })
+    });
+    res.end("Olá Mundo!");
 });
