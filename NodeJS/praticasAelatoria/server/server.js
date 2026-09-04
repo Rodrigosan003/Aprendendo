35965f030a3efba9afb5407ac9272aba2e5e555f
Rodrigosan003 <r.santanaa003@gmail.com>
2026-09-04 17:46:39 -0300

    NodeJs-praticasAleatorias-server

--- NodeJS/praticasAelatoria/server/server.js
+++ NodeJS/praticasAelatoria/server/server.js
@@ -6,3 +6,7 @@ const server = https.createServer((req, res) => {
     });
     res.end("Olá Mundo!");
 });
+
+server.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+});
